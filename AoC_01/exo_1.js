efcf64ea0fffce1f237398105ac7af32917014db
Alexandre Pianet <alexkid33@hotmail.com>
2025-12-03 01:08:53 +0100

    AoC2025 - exo1 - part 2

--- AoC_01/exo_1.js
+++ AoC_01/exo_1.js
@@ -3,8 +3,6 @@ const fs = require('fs');
 
 console.log("AoC 2025 - Jour 1 - Exo 1  \n");
 
-const testInput = fs.readFileSync('AoC_01/realInput.txt', 'utf8');
-run(testInput);
 
 
 function rotate(direction, nbClick, position) {
@@ -21,18 +19,49 @@ function rotate(direction, nbClick, position) {
     return position;
   }
 
-function run(input) {
+  function rotate2(direction, nbClick, position) {
+    let counter = 0;  
+    let nbZero = 0;
+    let newPosition = position;
+    if (direction === 'L') {
+          counter = position - nbClick;
+      } else if (direction === 'R') {
+          counter = position + nbClick;
+      }
+      newPosition = counter % 100;
+      nbZero = Math.abs(Math.trunc(counter / 100));
+      if(position !== 0 && Math.sign(counter) !== Math.sign(position)) nbZero+= 1;
+
+      if (newPosition > 99) newPosition = newPosition - 100;
+      if (newPosition < 0) newPosition = 100 + newPosition;
+      //console.log(`    ->  position ${position}, rotation ${direction}${nbClick} ==> counter ${counter},newPosition ${newPosition}, nbZero ${nbZero}`);
+      return [newPosition, nbZero];
+    }
+
+function run(input, part) {
     const lines = input.trim().split('\n');
     let count = 0;
     let currentPosition = 50;
+    let nbZero = 0;
     for (let i = 0; i < lines.length; i++) {
         const direction = lines[i][0];
         const nbClick = Number(lines[i].slice(1));
         //console.log(`Ligne ${i} : direction ${direction}, click ${nbClick}`);
-
-        currentPosition = rotate(direction,nbClick, currentPosition);
-        //console.log(`  -> Nouvelle position: ${currentPosition}`);
-        if(currentPosition === 0) count++;
+        if(part ===1){
+          currentPosition = rotate(direction,nbClick, currentPosition);
+          //console.log(`  -> Nouvelle position: ${currentPosition}`);
+          if(currentPosition === 0) count++;
+       }
+       if(part ===2){
+          [currentPosition, nbZero] = rotate2(direction,nbClick, currentPosition);
+          //if(currentPosition === 0) count++;
+          count += nbZero;
+       }
     }
-    console.log("Number of time that 0 position is reached:", count);
+    console.log("Part ",part," - Number of time that 0 position is reached:", count);
 }
+
+//const testInput = fs.readFileSync('AoC_01/testInput.txt', 'utf8');
+const testInput = fs.readFileSync('AoC_01/realInput.txt', 'utf8');
+run(testInput,1);
+run(testInput,2);
